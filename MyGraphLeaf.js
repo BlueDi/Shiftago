17d9cecaab8ee7e24c9ada0e6c761499c75bc875
Diogo Cruz <diogohmcruz@hotmail.com>
2017-09-30 15:29:01 +0100

    Criar os objetos base
    As folhas vao ler do xml os valores e criar os objetos.

--- MyGraphLeaf.js
+++ MyGraphLeaf.js
@@ -8,6 +8,17 @@ function MyGraphLeaf(graph, xmlelem) {
     var type = reader.getString(xmlelem, 'type');
     var args = reader.getString(xmlelem, 'args').split(" ");
 
-    if(type == 'rectangle')
-        var object = new Square(graph.scene, parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2]), parseFloat(args[3]));
+    this.object = new Cylinder(graph.scene, 1, 1, 1, 1, 1);
+
+    if (type == 'cylinder')
+        this.object = new Cylinder(graph.scene, parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2]), parseFloat(args[3]), parseFloat(args[4]));
+    else if (type == 'rectangle')
+        this.object = new Rectangle(graph.scene, parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2]), parseFloat(args[3]));
+    else if (type == 'sphere')
+        this.object = new Sphere(graph.scene, parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2]));
+    else if (type == 'square')
+        this.object = new Square(graph.scene, parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2]), parseFloat(args[3]));
+    else if (type == 'triangle')
+        this.object = new Triangle(graph.scene, parseFloat(args[0]), parseFloat(args[1]), parseFloat(args[2]), parseFloat(args[3]),
+            parseFloat(args[4]), parseFloat(args[5]), parseFloat(args[6]), parseFloat(args[7]), parseFloat(args[8]));
 }
